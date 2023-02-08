df39ce5207c5b016305f0eeff1bf20043f17e31c
Joey Tsui <joeytsui131@gmail.com>
2023-02-08 00:58:17 -0500

    setting for render

--- config/application.rb
+++ config/application.rb
@@ -42,5 +42,7 @@ module StadiumGoods
       secure: Rails.env.production?
       
     config.api_only = true
+
+    config.railties_order = [:all, :main_app]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
     resource :session, only: [:show, :create, :destroy]
   end
 
+  get "*path", to: "static_pages#frontend_index"
+
 end
