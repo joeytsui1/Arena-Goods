1b1582f9fccdeef561315f51d02776c2296460b2
Joey Tsui <joeytsui131@gmail.com>
2023-02-11 02:54:02 -0500

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,8 +35,8 @@ ApplicationRecord.transaction do
     description:
     "The Travis Scott x Fragment x Air Jordan 1 High OG “Military Blue” is a three way collaboration between the Houston rapper, the Japanese streetwear outfit, and Jordan Brand on Michael Jordan’s first signature shoe. Released alongside an Air Jordan 1 Low, the Travis Scott and Hiroshi Fujiwara-designed high-top Jordan 1 “Military Blue” combines the distinct aesthetics of both Scott and Fujiwara on the iconic retro basketball shoe. Supple Military Blue leather appears on the perforated toe and on the collar and heel. Contrasting white tumbled leather is located on the toe cap, forefoot, eyelets, and mid-panel. The oversized backwards black leather Swoosh is a call back to the styling of the hip-hop star’s first Air Jordan 1 collaboration from 2019. A black “Wings” logo resides on the collar. Industrial-style branding, as seen on fragment’s collaborations with Nike and Jordan Brand in the past, appears in black text on the medial side of the cream rubber midsole. Dual “Cactus Jack” and Fragment insignia can be found on the back of the left and right shoe, respectively. The Travis Scott x Fragment x Air Jordan 1 High OG “Military Blue” is one of the most desirable sneaker releases of 2021. Release date: July 29, 2021",
     price: 3410,
-    gender: "Mens",
-    released_date: "July 29, 2021",
+    # gender: "Mens",
+    # released_date: "July 29, 2021",
     image: "https://img.stadiumgoods.com/17/03/61/57/17036157_43024052_2048.jpg"
   )
 
