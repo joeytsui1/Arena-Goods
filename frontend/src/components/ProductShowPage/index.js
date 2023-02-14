273bd36429b74f9427daa61286cbd980bc014f31
Joey Tsui <joeytsui131@gmail.com>
2023-02-14 17:14:03 -0500

    minor bugfixes

--- frontend/src/components/ProductShowPage/index.js
+++ frontend/src/components/ProductShowPage/index.js
@@ -1,14 +1,16 @@
 import { useSelector, useDispatch } from "react-redux"
-import { useParams } from "react-router-dom"
+import { Redirect, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { getProduct, getProducts } from "../../store/product"
 import AllProductCarousel from "../Carousel/AllProductCarousel"
 import { makeCart } from "../../store/cart"
 import { fetchUserCart } from "../../store/cart"
+import { useHistory } from "react-router-dom"
 import "./ProductShowPage.css"
 
 const ProductShowPage = () => {
     const dispatch = useDispatch()
+    const history = useHistory()
     const { productId } = useParams()
     const [hide, setHide] = useState(true)
 
@@ -21,8 +23,8 @@ const ProductShowPage = () => {
     useEffect(() => {
         dispatch(getProduct(productId))
         dispatch(getProducts())
-        dispatch(fetchUserCart(currentUser.id))
-    }, [dispatch, productId])
+        currentUser ? dispatch(fetchUserCart(currentUser.id)) : dispatch(() => 1)
+    }, [dispatch, productId, cart.length])
 
     if (product === undefined) {
         return <>Still loading...</>;
@@ -33,7 +35,9 @@ const ProductShowPage = () => {
         setHide(!hide)
     } 
 
-    console.log(currentUser)
+    const redirectLogin = () => {
+        history.push("/login")
+    }
 
     const addToCart = (e) => {
         const cart_item = {
@@ -42,7 +46,7 @@ const ProductShowPage = () => {
             size: 8.5
         }
         dispatch(makeCart(cart_item))
-        window.location.reload()
+        // window.location.reload()
     }
 
     return (
@@ -67,7 +71,7 @@ const ProductShowPage = () => {
                     <button className="size-option" onClick={handleClick}>Select Size</button>
                     {hide ? null : 
                     <div>is open</div> }
-                    <button onClick={addToCart}className="add-to-cart">ADD TO CART</button>
+                    <button onClick={currentUser ? addToCart : redirectLogin}className="add-to-cart">ADD TO CART</button>
                 </div>
             </div>
         </div>
