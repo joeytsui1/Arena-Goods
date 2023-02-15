542763ca5da361e11c02743a4a4d61867c0c4e09
Joey Tsui <joeytsui131@gmail.com>
2023-02-15 14:14:01 -0500

    render testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,6 @@ ApplicationRecord.transaction do
   # For easy testing, so that after seeding, the first `User` has `id` of 1
   ApplicationRecord.connection.reset_pk_sequence!("users")
   ApplicationRecord.connection.reset_pk_sequence!("products")
-
   puts "Creating users..."
   # Create one user with an easy to remember username, email, and password:
   User.create!(
